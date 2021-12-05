7209fb564802ab3fbe10e2e2ab1c87987f75909f
Martin Namukombo <mastadict@gmail.com>
2021-12-05 19:01:21 +0000

    Final commit

--- src/main/java/uk/ac/ed/inf/DeliveryPlanner.java
+++ src/main/java/uk/ac/ed/inf/DeliveryPlanner.java
@@ -280,11 +280,10 @@ public class DeliveryPlanner {
    */
   public void generatePathMap() {
     FeatureCollection featureCollection = FeatureCollection.fromFeature(generatePathLineString());
-    String filePrefix = "src/main/java/uk/ac/ed/inf/";
     String[] dateParts = deliveryDate.toString().split("-");
     String dateString = dateParts[2] + "-" + dateParts[1] + "-" + dateParts[0];
     try {
-      FileWriter myWriter = new FileWriter(filePrefix + "drone-" + dateString + ".geojson");
+      FileWriter myWriter = new FileWriter("drone-" + dateString + ".geojson");
       myWriter.write(featureCollection.toJson());
       myWriter.close();
     } catch (IOException e) {
